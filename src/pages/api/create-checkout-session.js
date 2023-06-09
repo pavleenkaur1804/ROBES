679ecb5ad1640885fb43f0150b0e75d8b98bff28
Pavleen <pavleenkaur1804>
2023-06-09 14:38:07 +0530

    create checkout session logs

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-
+console.log('stripe', stripe)
 export default async (req, res) => {
     try {
+        console.log('entering Checkout')
         const { items, email } = req.body
 
         const transformedItems = items.map(item => ({
@@ -18,7 +19,7 @@ export default async (req, res) => {
                 unit_amount: item.price * 100,
             }
         }))
-
+        console.log('transformedItems', transformedItems)
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ["card"],
             // shipping_options: ['shr_1Mwl9pSJxRLy98VhcVoLmxMk'],
@@ -34,6 +35,8 @@ export default async (req, res) => {
                 images: JSON.stringify(items.map(item => item.image))
             }
         })
+        console.log('session',session)
+        console.log('sessionurl',session.url)
         res.status(200).json({ url: session.url })
     } catch (err) {
         console.log('err', err)
